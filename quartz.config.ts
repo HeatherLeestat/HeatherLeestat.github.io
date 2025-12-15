3850c59e42c0b758c82316ce69ae2090e0e504d7
HeatherLeestat <dukongi@hanmail.net>
2025-12-15 12:42:34 +0900

    Fix baseUrl for GitHub Pages (user site)

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   siteUrl: "https://heatherleestat.github.io",
-  baseUrl: "/",
+  baseUrl: "",
   configuration: {
     pageTitle: "Quartz 4",
     pageTitleSuffix: "",
